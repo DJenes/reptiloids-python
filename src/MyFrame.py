47b784da5d6024ece93f48daea1387f6ea23f262
Dasha <gameampere@gmail.com>
2018-10-29 21:40:53 +0200

    initial commit

---
+++ src/MyFrame.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*- 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Aug  8 2018)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+
+###########################################################################
+## Class MyFrame
+###########################################################################
+
+class MyFrame(wx.Frame):
+
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"Решение системы линейных уравнений",
+                          pos=wx.DefaultPosition, size=wx.Size(707, 400),
+                          style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+
+        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+
+        self.gbSizer1 = wx.GridBagSizer(0, 0)
+        self.gbSizer1.SetFlexibleDirection(wx.BOTH)
+        self.gbSizer1.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+
+        self.num_of_lines = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(100, -1), 0)
+        self.num_of_lines.SetMaxLength(1)
+        self.gbSizer1.Add(self.num_of_lines, wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.m_staticText1 = wx.StaticText(self, wx.ID_ANY, u"N уравнений", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText1.Wrap(-1)
+
+        self.gbSizer1.Add(self.m_staticText1, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.m_button2 = wx.Button(self, wx.ID_ANY, 'Построить', wx.DefaultPosition, wx.Size(100, -1), 0)
+        self.gbSizer1.Add(self.m_button2, wx.GBPosition(2, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.m_button3 = wx.Button(self, wx.ID_ANY, 'Решить', wx.DefaultPosition, wx.Size(100, -1), 0)
+        self.gbSizer1.Add(self.m_button3, wx.GBPosition(3, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.SetSizer(self.gbSizer1)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
+
+        # Connect Events
+        self.m_button2.Bind(wx.EVT_BUTTON, self.init_grid)
+        self.m_button3.Bind(wx.EVT_BUTTON, self.solve)
+
+    def __del__(self):
+        pass
+
+    # Virtual event handlers, overide them in your derived class
+    def init_grid(self, event):
+        event.Skip()
+
+    def solve(self, event):
+        event.Skip()
---
+++ src/main.py
@@ -0,0 +1,50 @@
+import numpy
+import wx
+import logging
+from MyFrame import MyFrame
+
+
+class MainFrame(MyFrame):
+    def __init__(self, *args, **kwargs):
+        MyFrame.__init__(self, *args, **kwargs)
+        self.status = False
+        self.cells = []
+
+    def init_grid(self, event):
+        num = int(self.num_of_lines.GetValue())
+        if not self.status:
+            self.status = True
+            self.create_grid(num)
+        logging.warning(num)
+
+    def create_grid(self, count):
+        for x in range(0, count):
+            self.cells.append([])
+            for y in range(0, count+1):
+                element = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(50, -1), 0)
+                self.gbSizer1.Add(element, wx.GBPosition(1 + x, 1 + y), wx.GBSpan(1, 1), wx.ALL, 5)
+                self.cells[x].append(element)
+        self.Layout()
+
+    def solve(self, event):
+        v1 = [float(self.cells[x][-1].GetValue()) for x in range(0, len(self.cells))]
+        m1 = []
+        for x in range(0, len(self.cells)):
+            m1.append([])
+            for y in range(0, len(self.cells)):
+                m1[x].append(float(self.cells[x][y].GetValue()))
+        answer = numpy.linalg.solve(m1, v1)
+        logging.warning(v1)
+        logging.warning(m1)
+        logging.warning(answer)
+
+
+app = wx.App()
+frame = MainFrame(None)
+frame.Show()
+app.MainLoop()
+
+# TODO: autosizing
+# TODO: validation of user input
+# TODO: logging to file
+# TODO: tests
